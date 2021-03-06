7449d7faf247d1842330644c4edcc527fb628b1d
FamiliarStrang3r <strongbaby1337@gmail.com>
2021-03-06 02:20:07 +0200

    gameplay scene + basic ai

---
+++ Assets/Scripts/SimpleAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour
+{
+    private CarInputHandler player = null;
+
+    private TopDownCarController controller = null;
+
+    private void Start()
+    {
+        player = FindObjectOfType<CarInputHandler>();
+
+        controller = GetComponent<TopDownCarController>();    
+    }
+
+    private void Update()
+    {
+        Vector3 dir = (player.transform.position + player.transform.up * 2) - transform.position;
+
+        dir = transform.InverseTransformDirection(dir);
+
+        controller.SetInputVector(dir.normalized);
+    }
+}
---
+++ Assets/Scripts/SimpleCameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+
+    [SerializeField] private float amount = 1f;
+
+    private void LateUpdate()
+    {
+        var pos = Vector3.Lerp(transform.position, target.position, amount * Time.deltaTime);
+        transform.position = pos;
+    }
+}
