0f7c75a2d93cad501354808c7ec65afa573e1e24
Dima <strongbaby1337@gmail.com>
2022-04-10 00:55:19 +0300

    waypoint follower ai

---
+++ Assets/Scripts/WaypointFollowerAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollowerAI : SimpleAI
+{
+    [SerializeField] private WaypointPath m_Path = null;
+
+    [SerializeField] private int m_CurrentIndex = 0;
+
+    [SerializeField] private float m_DistanceThreshold = 1;
+
+    protected override Vector3 GetChasePoint()
+    {
+        Vector3 point = m_Path.GetPoint(m_CurrentIndex);
+
+        if (Vector3.Distance(transform.position, point) < m_DistanceThreshold)
+        {
+            if (m_CurrentIndex + 1 == m_Path.Count())
+            {
+                m_CurrentIndex = 0;
+            }
+            else
+            {
+                m_CurrentIndex++;
+            }
+        }
+
+        return point;
+    }
+}
